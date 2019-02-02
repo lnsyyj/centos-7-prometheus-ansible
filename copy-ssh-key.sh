7ac6616ee2880478539b5f86ceef8be2962959da
root <root@ansible-dev.novalocal>
2019-02-02 11:12:51 +0800

    update

--- copy-ssh-key.sh
+++ copy-ssh-key.sh
@@ -2,7 +2,8 @@
 #set -x
 
 COPY_SSH_RSA_PUB_NODE=(10.121.123.1 10.121.123.2 10.121.123.3 10.121.123.4 10.121.123.5 10.121.123.6 10.121.123.7 10.121.123.8 10.121.123.9 10.121.123.10 10.121.123.11 10.121.123.12 10.121.123.13 10.121.123.14 10.121.123.15 10.121.123.16 10.121.123.17 10.121.123.18 10.121.123.19 10.121.123.20 10.121.123.21 10.121.123.22 10.121.123.23 10.121.123.24 10.121.123.25 10.121.123.26 10.121.123.27 10.121.123.28 10.121.123.29 10.121.123.30 10.121.123.31 10.121.123.32 10.121.123.33 10.121.123.34 10.121.123.35 10.121.123.36)
-
+COPY_CEPH_EXPORTER_NODE=(10.121.123.14 10.121.123.18 10.121.123.19)
+PROMETHEUS_EXPORTER_DIR="/home/prometheus"
 MACHINE_PASSWORD="password"
 
 function copy_ssh_rsa_pub() {
@@ -20,5 +21,64 @@ function copy_ssh_rsa_pub() {
         done
 }
 
-yum install -y expect
-copy_ssh_rsa_pub
+function create_exporter_dir() {
+	for data in ${COPY_SSH_RSA_PUB_NODE[@]}
+	do
+		ssh "root@${data}" "mkdir -p ${PROMETHEUS_EXPORTER_DIR}"
+	done
+}
+
+function copy_node_exporter() {
+	for data in ${COPY_SSH_RSA_PUB_NODE[@]}
+	do
+		scp node_exporter root@${data}:${PROMETHEUS_EXPORTER_DIR}
+	done
+}
+
+function copy_node_exporter_systemd_conf() {
+	for data in ${COPY_SSH_RSA_PUB_NODE[@]}
+	do
+		scp node_exporter.service root@${data}:/usr/lib/systemd/system
+	done
+}
+
+function start_node_exporter() {
+        for data in ${COPY_SSH_RSA_PUB_NODE[@]}
+        do
+                ssh "root@${data}" "systemctl enable node_exporter.service"
+		ssh "root@${data}" "systemctl start node_exporter.service"
+        done
+}
+
+function copy_ceph_exporter() {
+        for data in ${COPY_CEPH_EXPORTER_NODE[@]}
+        do
+                scp ceph_exporter root@${data}:${PROMETHEUS_EXPORTER_DIR}
+        done
+}
+
+function copy_ceph_exporter_systemd_conf() {
+        for data in ${COPY_CEPH_EXPORTER_NODE[@]}
+        do
+                scp ceph_exporter.service root@${data}:/usr/lib/systemd/system
+        done
+}
+
+function start_ceph_exporter() {
+        for data in ${COPY_CEPH_EXPORTER_NODE[@]}
+        do
+                ssh "root@${data}" "systemctl enable ceph_exporter.service"
+                ssh "root@${data}" "systemctl start ceph_exporter.service"
+        done
+}
+
+#yum install -y expect
+#copy_ssh_rsa_pub
+#create_exporter_dir
+#copy_node_exporter
+#copy_node_exporter_systemd_conf
+#start_node_exporter
+
+#copy_ceph_exporter
+#copy_ceph_exporter_systemd_conf
+start_ceph_exporter
